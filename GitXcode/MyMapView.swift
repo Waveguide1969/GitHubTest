9c2ae9152f6b411dff0e6d19afe6c25fc83f8be8
Ralf Matz <ralfmatz@ralfs-air.fritz.box>
2017-08-04 14:46:33 +0200

    Added MapViewController

---
+++ GitXcode/MyMapView.swift
@@ -0,0 +1,21 @@
+//
+//  MyMapView.swift
+//  GitXcode
+//
+//  Created by Ralf Matz on 04.08.17.
+//  Copyright © 2017 Ralf Matz. All rights reserved.
+//
+
+import UIKit
+
+class MyMapView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
